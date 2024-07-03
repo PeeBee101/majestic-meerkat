f128d4df31841754365f720c12af8ef9340c1566
Philippe <p33b33@live.co.uk>
2024-07-03 23:10:38 +0100

    finished menu for desktop

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 jQuery(document).ready(function($) {
+    // Burger menu class toggles for CSS changes
     $('.hamburger').click(function() {
         $(this).toggleClass('is-active');
-        $('html, body').toggleClass('is-hidden');
-        $('.menu').toggleClass('is-hidden');
+        $('body').toggleClass('open-menu')
         $('.logo a img').toggleClass('is-active');
+    });
+
+    // Scroll function to change the menu
+    function toggleScrolledClass() {
+        if ($(window).scrollTop() > 100) {
+            $('.logo-block, .menu-block').addClass('scrolled');
+        } else if (!$('body').hasClass('open-menu')) {
+            $('.logo-block, .menu-block').removeClass('scrolled');
+        }
+    }
+
+    // Check the scroll position when the page loads
+    toggleScrolledClass();
+
+    // Attach the scroll event listener
+    $(window).scroll(function() {
+        toggleScrolledClass();
     });
 });
